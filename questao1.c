18beb30551396fb11dd74bc1a4b566c5f492fc62
caiomoreirab <caiomoreira335@gmail.com>
2024-11-18 12:09:22 -0300

    First Commit!

--- questao1.c
+++ questao1.c
@@ -1,40 +1,105 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-//criar o vetor dentro da funçao 
 
 
 
-void imprimeMaior(int n) {
-    int* v = (int*)malloc(n * sizeof(int));
 
 
-    if (v != NULL) {
-        for (int i =0; i < n; i++ ){
-        printf("Digite um valor para a posição:");
-        scanf("%d", &v[i]);
+
+//1. Faça um algoritmo que recebe um número inteiro n, e um vetor de inteiros com n
+//posições e retorna a soma dos números lidos percorrendo o vetor.
+//ProtóSpo: int SomaElmVetor(int *v, int n)
+
+// a) Imprime o maior valor do vetor;
+// ProtóZpo: void ImprimeMaiorValor (int n);
+
+#include <stdio.h>                                                                                                             
+
+int SomaElmVetor(int *v, int n) {
+    int i = 0;
+    int soma = 0;
+    while (i < n){
+        soma += v[i];
+        i++;
     }
-    int maior = v[0];
-
-    for (int j = 1; j < n; j++) {
-        if(v[j] > maior) {
-            maior = v[j];
-        }
-    }
-
-    printf("%d", maior);
-
-    } else {
-        printf("Error");
-    }
-
+    return soma;
 }
-
 
 
 
 int main() {
-    imprimeMaior(6);
+    int vetor[] = {1, 2, 3, 4, 5}; // Exemplo de vetor
+    int n = 5; // Número de elementos no vetor
+
+    int resultado = SomaElmVetor(vetor, n);
+    printf("A soma dos elementos do vetor é: %d\n", resultado);
 
     return 0;
 }
+
+
+
+
+
+// b) Imprime a soma dos números;
+// ProtóZpo: void ImprimeSomaDosNumeros (int n);
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void imprimeSomaDosNumeros(int n) {
+
+    int *v = (int *)malloc(sizeof(int) * n);
+    
+    if (v == NULL) {
+        printf("Erro ao alocar memória.\n");
+        return;
+    }
+    
+    for (int i = 0; i < n; i++) {
+        printf("Digite o valor do vetor na posição %d: ", i);
+        scanf("%d", &v[i]);
+    }
+
+    int contSoma = 0;
+    for (int i = 0; i < n; i++) {
+        contSoma += v[i];
+    }
+
+
+    printf("Soma dos valores do vetor: %d\n", contSoma);
+    free(v);
+}
+
+
+
+
+
+
+
+
+
+
+// c) Imprime o elemento da posição teto (n/2)
+// ProtóZpo: void ImprimeElementoDoMeio (int n);
+
+
+void ImprimeElementoDoMeio (int n) {
+    int *v = (int *)malloc(sizeof(int) * n);
+    
+    if (v == NULL) {
+        printf("Erro ao alocar memória.\n");
+        return;
+    }
+    
+    for (int i = 0; i < n; i++) {
+        printf("Digite o valor do vetor na posição %d: ", i);
+        scanf("%d", &v[i]);
+    }
+
+    int pos = (n + 1) / 2 - 1; // Posição do meio
+    
+    printf("Elemento da posição teto(n/2): %d\n", v[pos]);
+
+    free(v);
+}
+
+
